b98bbebd5a5e1b0506484bef45cf5a2c40671a64
Juhamikael <contact.juhamusic@gmail.com>
2023-03-09 23:59:22 +0200

    Try Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -12,6 +10,11 @@ import preact from "@astrojs/preact";
 import lit from "@astrojs/lit";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), preact(), lit()],
+  output: "server",
+  adapter: netlify()
 });
